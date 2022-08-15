01642aea1fec980b8bf5c14839a8f7a56ac46e05
tomonariasai <t.asai@4nonome.com>
2022-08-15 21:24:52 +0900

    plan.stories.js仮

---
+++ src/stories/molecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Item_plan from "../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default { title: "Molecules/plan" };
+
+export const plan = () => Item_plan;
